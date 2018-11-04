675f479f69c215afb16a65f803b29b96b31162ed
tungdt <daotung253@gmail.com>
2018-11-04 21:04:25 +0700

    first commit

---
+++ resources/assets/js/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+            <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+            <div className="panel panel-default">
+            <div className="panel-heading">Example </div>
+
+        <div className="panel-body">
+            I'm an example component11!
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    );
+    }
+}
+
+export default App;
